30dea43a7496ceadaa243438828d32be8ed1132f
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-09-02 01:12:47 +0530

    setup project controller

---
+++ Server/src/controller/user.controller.js
@@ -0,0 +1,19 @@
+const resgister = (req,res)=>{
+
+}
+const login = (req,res)=>{
+
+}
+const logout = (req,res)=>{
+
+}
+const getProfile = (req,res)=>{
+
+}
+
+module.exports= {
+    resgister,
+    login,
+    logout,
+    getProfile
+}
