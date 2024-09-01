b8eb28e5b76f99eb2b0c7b37f446d3493b091be3
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-09-02 00:37:29 +0530

    setup project files - part 1

---
+++ Server/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieparse from "cookie-parser";
+
+const app = express()
+
+app.use(express.json());
+
+app.use(cors(
+    {
+        origin:[process.env.FRONTEND_URL],
+        credentials:true
+    }
+));
+
+app.use(cookieparse())
+
+app.use('/ping',(req,res)=>{
+    app.send("Pong");
+});
+
+export default app;
