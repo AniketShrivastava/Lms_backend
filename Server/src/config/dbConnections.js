00ea9e8bb234f39006a0523de072a31efaa7612f
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-09-02 22:51:39 +0530

    setup db connection

---
+++ Server/src/config/dbConnections.js
@@ -0,0 +1,19 @@
+import mongoose, { connection } from "mongoose";
+mongoose.set("strictQuery",false);
+
+const connectToDB = async ()=>{
+    try {
+        const {connection} = await mongoose.connect(
+            process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/lms'
+        );
+        if(connection){
+            console.log(`Connecting to MongoDB: ${connection.host}`)
+        }
+
+    } catch (error) {
+        console.log(error);
+        console.exit(1);
+    }
+}
+
+module.exports = connectToDB;
