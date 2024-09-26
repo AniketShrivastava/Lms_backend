52c8520aaedd1a86c69c12bac3d545422e7c5736
Aniket shrivastava <aniket.shrivastava91@gmail.com>
2024-09-27 00:52:45 +0530

    Added basic layout and integrated to homepage

--- lms-frontend-en/src/App.jsx
+++ lms-frontend-en/src/App.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import toast from 'react-hot-toast'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+
 
 function App() {
   
 
-  useEffect(()=>{
-    toast.success("hello")
-  })
+  
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    
   )
 }
 
